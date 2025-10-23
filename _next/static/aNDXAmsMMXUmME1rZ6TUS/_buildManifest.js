ea70db28bfe0452d8e3fe0b1b72a64dca063051d
Kutikov Pasha <kutikovpasha@gmail.com>
2025-10-23 23:31:35 +0600

    Deploy [skip ci]

--- _next/static/aNDXAmsMMXUmME1rZ6TUS/_buildManifest.js
+++ _next/static/aNDXAmsMMXUmME1rZ6TUS/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/russian-cuisine-festival//_next/:path+",
-        "destination": "/russian-cuisine-festival/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
